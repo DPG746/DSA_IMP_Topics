d2fcb42393debdc8a3a3297951a305830604264c
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-05-05 05:33:54 +0530

    Add main method for CircularQueue demonstration
    Implement main method to demonstrate CircularQueue operations.

---
+++ CircularQueueMain.java
@@ -0,0 +1,23 @@
+public class CircularMain {
+    public static void main(String[] args){
+        CircularQueue circularQueue=new CircularQueue(5);
+        circularQueue.enqueue(10);
+        circularQueue.enqueue(20);
+        circularQueue.enqueue(6);
+        System.out.println("size of the queue: "+circularQueue.size());
+        circularQueue.display();
+        circularQueue.enqueue(100);
+        circularQueue.display();
+        System.out.println(circularQueue.dequeue());
+        circularQueue.display();
+        circularQueue.enqueue(130);
+        circularQueue.display();
+        circularQueue.enqueue(150);
+        circularQueue.display();
+        circularQueue.enqueue(190);
+        circularQueue.display();
+       // System.out.println(circularQueue.dequeue());
+        circularQueue.display();
+
+    }
+}
