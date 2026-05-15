8a381d0798a95773ecc41cba1e70b75c7d989f34
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-05-15 16:49:25 +0530

    Create StackLinkedList.java

---
+++ StackLinkedList.java
@@ -0,0 +1,42 @@
+
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data=data;
+        this.next=null;
+
+    }
+
+
+}
+class stack1{
+    Node top;
+    public stack1(){
+        this.top=null;
+
+    }
+    void push(int element){
+        Node temp=new Node(element);
+        temp.next=top;
+        top=temp;
+
+
+    }
+    int pop(){
+        int ele=top.data;
+        top=top.next;
+        return ele;
+    }
+
+
+}
+public class StackLinkedList {
+    public static void main(String[] args) {
+        stack1 s=new stack1();
+        s.push(10);
+        s.push(20);
+        System.out.println(s.pop());
+        System.out.println(s.pop());
+    }
+}
