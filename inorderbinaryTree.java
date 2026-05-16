1959331793c3ec5a59596a4d9335e63b7671c000
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-05-16 23:29:55 +0000

    inorder using stack

---
+++ inorderbinaryTree.java
@@ -0,0 +1,36 @@
+class treeNode{
+    int data;
+    treeNode left;
+    treeNode right;
+    
+    treeNode(int data){
+        this.data=data;
+        this.left=null;
+        this.right=null;
+    }
+}
+class Tree{
+    static void inorder(treeNode root){
+        if(root!=null){
+            inorder(root.left);
+            System.out.println(root.data+ " ");
+            inorder(root.right);
+
+        }
+
+    }
+
+}
+public class inorderbinaryTree{
+    public static void main(String[] args){
+       
+        treeNode root=new treeNode(10);
+        root.left=new treeNode(20);
+        root.right=new treeNode(30);
+        root.left.left=new treeNode(40);
+        root.right.left=new treeNode(50);
+        root.right.right=new treeNode(60);
+        Tree.inorder(root);
+
+    }
+}
