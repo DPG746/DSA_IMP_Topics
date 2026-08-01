8938b43a13d05f1e39fea3cbdf289e1b83acde06
Durga Prasad Giri <104110048+DPG746@users.noreply.github.com>
2026-08-01 11:39:41 +0530

    Create FixedSlidingWindow.java

---
+++ FixedSlidingWindow.java
@@ -0,0 +1,15 @@
+// Fixed Slding Window Pattern:
+
+// Build first window
+for (int i = 0; i < k; i++) { }
+
+// Sliding
+int start = 0;
+int end = k;
+
+while (end < n) {
+    // remove nums[start]
+    // add nums[end]
+    start++;
+    end++;
+}
